2183e78e02fd062c54c20476aba227f86a0859fb
PsychoKramps <Fraiden70@gmail.com>
2022-09-05 11:12:10 -0500

    token actu

--- src/helpers/datosGET.js
+++ src/helpers/datosGET.js
@@ -1,8 +1,10 @@
-const TOKEN = "Bearer BQDNbPDvb5-OSskcoNWJIgpRxa9m9Q-eQWz0ZDiU7heSJgJQSjfjPbhee1y0mB--AlcDDsHbL6hEkTjwah8Byr07Clz46O4RDgiz5x7Xg-2X3il4u3vGq2FR4jib1nxI2iIKUFxQia8X2f-R1mt-44t254LfvtC40r4wwNmCOczHRs56mDCcQw5F9BlYsMBwZUA"
+import {obtenerTOKEN} from "../services/generarTOKEN.js"
+
+let TOKEN = await obtenerTOKEN()
 
 export const PETICION = {
     method: 'GET',
     headers: {
-        Authorization: TOKEN
+        Authorization: TOKEN.token_type+" "+TOKEN.access_token
     }
 }
--- src/services/generarTOKEN.js
+++ src/services/generarTOKEN.js
@@ -1,4 +1,3 @@
-
 const URI = "https://accounts.spotify.com/api/token"
 const DATO1 = "grant_type=client_credentials"
 const DATO2 = "client_id=509da18a4b934c3bba1623523158c976"
@@ -6,7 +5,13 @@ const DATO3 = "client_secret=224fc6ff0c0c479dbc9421d0db759b28"
 const PETICION = {
     method: "POST",
     headers:{
-
+        "Content-Type": "application/x-www-form-urlencoded"
     },
     body:DATO1+"&"+DATO2+"&"+DATO3
 }
+export async function obtenerTOKEN(){
+    let response = await fetch(URI,PETICION)
+    return await response.json()
+}
+// let Token = await obtenerTOKEN()
+// console.log(Token.access_token)
